970d6a1d8da51a74178e535874a693137785c1b2
AppClinica <appclinicaprueba@gmail.com>
2025-07-06 14:11:44 -0500

    Cambios

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/bienvenida', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'bienvenida.html'));
+});
+
 // Login
 app.post('/login', (req, res) => {
   const { usuario, password } = req.body;
